701f52537e9f50c32c364624e17c7a7100848d61
Tim Ng <timng@tims-macbook-pro.local>
2019-03-15 18:35:50 +0800

    Initial Commit

---
+++ StretchyHeader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StretchyHeader
+//
+//  Created by Tim Ng on 3/15/19.
+//  Copyright © 2019 Tim Ng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
